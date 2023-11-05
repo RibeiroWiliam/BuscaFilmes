ac807a0288a4b1b05ed287ec4f0642c9e72f41e6
Wiliam Ribeiro <wiliamribeiro.tj@gmail.com>
2023-11-04 21:47:31 -0300

    Consumo da API trazendo as fotos

--- buscafilmes-app/src/app/card.js
+++ buscafilmes-app/src/app/card.js
@@ -1,7 +1,17 @@
-export default function Card() {
-    return (
-        <div>
+import styles from './card.css'
+import fetch from 'node-fetch';
 
+export default function Card(movie) {
+    console.log(movie)
+    const url = "https://image.tmdb.org/t/p/w500" + movie.movie.poster_path;
+
+    return (
+        <div className={styles.cardBox}>
+            <div className={styles.imgBox}>
+                <img src={url} />
+            </div>
+            {movie.title}
+            <a className={styles.Link}>Ver Mais</a>
         </div>
     )
 }
--- buscafilmes-app/src/app/page.js
+++ buscafilmes-app/src/app/page.js
@@ -1,16 +1,70 @@
-import BtnBuscar from './btnBuscar'
+"use client"
+
+import Card from './card'
 import Navbar from './navbar'
 import styles from './page.module.css'
+import React, { createContext, useContext, useState } from 'react';
+
+export const MoviesContext = createContext([]);
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
   return (
-    <body>
-      <Navbar />
-      <main className={styles.mainBox}>
-        <div className={styles.searchBox}>
-          <BtnBuscar />
-        </div>
-      </main>
-    </body>
+    <MoviesContext.Provider value={movies}>
+      <body>
+        <Navbar />
+        <main className={styles.mainBox}>
+          <section className={styles.sectionBox}>
+            <div className={styles.searchBox}>
+              <BtnBuscar setMovies={setMovies} />
+            </div>
+          </section>
+          <MoviesGrid />
+        </main>
+      </body>
+    </MoviesContext.Provider>
+  )
+
+
+}
+
+function BtnBuscar({ setMovies }) {
+  let movies = useContext(MoviesContext);
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const apiKey = "afbeee5dc8a56d84b3457702342ba299"
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        movies = data.results;
+        setMovies(movies);
+      });
+  }
+
+  return (
+    <div>
+      <form className={styles.searchBox} onSubmit={handleFormSubmit}>
+        <input type="search" name="" className={styles.searchInput} value={query} onChange={handleInputChange} placeholder="Digite o nome de um filme..." />
+        <button type="submit" className={styles.btnBuscar}><i class='bx bx-search'></i></button>
+      </form>
+    </div>
+  )
+}
+
+function MoviesGrid() {
+  const movies = useContext(MoviesContext);
+
+  return (
+    <section className={styles.movieList}>
+      {movies.map((movie) => (
+        <Card movie={movie} />
+      ))}
+    </section>
   )
 }
