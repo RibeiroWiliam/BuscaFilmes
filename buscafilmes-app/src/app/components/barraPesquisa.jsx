30d48e4c408929eadd9e74124b7850ab66065b6e
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2023-11-12 22:00:43 -0300

    Fazendo a navbar funcionar

--- buscafilmes-app/src/app/components/barraPesquisa.jsx
+++ buscafilmes-app/src/app/components/barraPesquisa.jsx
@@ -1,78 +1,107 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 
-import styles from '../css/barraPesquisa.css'
+import styles from "../css/barraPesquisa.css";
 
 export default function BarraPesquisa() {
-    const [query, setQuery] = useState('');
-    const [language, setLanguage] = useState([]);
-    const [year, setYear] = useState([]);
-    const [showFilters, setShowFilters] = useState(false);
-    const router = useRouter();
+  const [query, setQuery] = useState("");
+  const [language, setLanguage] = useState([]);
+  const [year, setYear] = useState([]);
+  const [showFilters, setShowFilters] = useState(false);
+  const router = useRouter();
 
-    const handleInputChange = (event) => {
-        setQuery(event.target.value);
+  const input = useRef();
+
+  useEffect(() => {
+    if (query != undefined) {
+      input.value = router.query;
+      console.log(router);
     }
+  }, []);
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        if (router.pathname == "/search") {
-            router.push(`../${query}/${language}/${year}`);
-        }
-        else {
-            router.push(`/search/${query}/${language}/${year}`);
-        }
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (query && router.pathname == "/search") {
+      router.push(`../${query}/${language}/${year}`);
+    } else if (query) {
+      router.push(`/search/${query}/${language}/${year}`);
     }
+  };
 
-    function handleFilterClick() {
-        if (showFilters) {
-            setShowFilters(false);
-        }
-        else {
-            setShowFilters(true);
-        }
+  function handleFilterClick() {
+    if (showFilters) {
+      setShowFilters(false);
+    } else {
+      setShowFilters(true);
     }
+  }
 
-    function handleClose() {
-        setShowFilters(false);
-    }
+  function handleClose() {
+    setShowFilters(false);
+  }
 
-    return (
-        <div>
-            <form onSubmit={handleFormSubmit}>
-                <div class="searchBox">
-                    <input type="search" class="searchInput" name="" value={query} onChange={handleInputChange} placeholder="Digite o nome de um filme..." />
-                    <button type="submit" class="btnSearch"><i class='bx bx-search'></i></button>
-                </div>
-                <button type="button" class="btnFilters" onClick={handleFilterClick}><i class='bx bx-slider'></i></button>
-            </form>
-            {showFilters && (
-                <div class="filtersBox">
-                    <h2>Escolha seus filtros:</h2>
-                    <form >
-                        <label for='language'> Idioma: </label>
-                        <select value={language} name='language'>
-                            <option value="pt-BR" defaultChecked> Português (Brasil) </option>
-                            <option value="en-US">Inglês</option>
-                        </select>
-
-                        <label for='year'> Ano de Lançamento: </label>
-                        <input type="number" min="1900" max="2023" value={year} name="year" />
-
-                        <button type="submit" onClick={handleFormSubmit}>Aplicar filtros</button>
-                        <button type="button" class="btnClose" onClick={handleClose}>Fechar</button>
-                    </form>
-                </div>
-            )}
-
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <div class="searchBox">
+          <input
+            ref={input}
+            type="search"
+            class="searchInput"
+            name=""
+            value={query}
+            onChange={handleInputChange}
+            placeholder="Digite o nome de um filme..."
+          />
+          <button type="submit" class="btnSearch">
+            <i class="bx bx-search"></i>
+          </button>
         </div>
-    )
+        <button type="button" class="btnFilters" onClick={handleFilterClick}>
+          <i class="bx bx-slider"></i>
+        </button>
+      </form>
+      {showFilters && (
+        <div class="filtersBox">
+          <h2>Escolha seus filtros:</h2>
+          <form>
+            <label for="language"> Idioma: </label>
+            <select value={language} name="language">
+              <option value="pt-BR" defaultChecked>
+                {" "}
+                Português (Brasil){" "}
+              </option>
+              <option value="en-US">Inglês</option>
+            </select>
+
+            <label for="year"> Ano de Lançamento: </label>
+            <input
+              type="number"
+              min="1900"
+              max="2023"
+              value={year}
+              name="year"
+            />
+
+            <button type="submit" onClick={handleFormSubmit}>
+              Aplicar filtros
+            </button>
+            <button type="button" class="btnClose" onClick={handleClose}>
+              Fechar
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
 
 function Tag(tag) {
-    return (
-        <div>{tag}</div>
-    )
+  return <div>{tag}</div>;
 }
--- buscafilmes-app/src/app/components/navbar.jsx
+++ buscafilmes-app/src/app/components/navbar.jsx
@@ -1,30 +1,63 @@
-import Link from 'next/link'
-import styles from '../css/navbar.css'
+"use client";
+
+import Link from "next/link";
+import styles from "../css/navbar.css";
+import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-    return (
-        <header>
-            <nav class="navbar navbar-expand-lg fixed-top" >
-                <div class="container-fluid" >
-                    <Link class="navbar-brand" href="/">BuscaFilmes</Link>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" href="/">Home</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="/search">Filmes</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="/about">Sobre</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
+  const home = useRef(),
+    about = useRef();
+
+  const router = useRouter;
+
+  useEffect(() => {
+    switch (router.pathname) {
+      case "/":
+        home.class.toggle("active");
+        home.aria - current.toggle("page");
+        break;
+      case "/about":
+        about.class.toggle("active");
+        about.aria - current.toggle("page");
+        break;
+    }
+  }, []);
+
+  return (
+    <header>
+      <nav class="navbar navbar-expand-md fixed-top">
+        <div class="container-fluid">
+          <Link class="navbar-brand" href="/">
+            BuscaFilmes
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link ref={home} class="nav-link" href="/">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link ref={about} class="nav-link" href="/about">
+                  Sobre
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
