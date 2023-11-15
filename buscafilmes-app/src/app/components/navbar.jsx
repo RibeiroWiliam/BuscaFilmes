c83474a84a20e9c4f46abaf6c5410f39ffa7a8b3
RibeiroWiliam <wiliamribeiro.tj@gmail.com>
2023-11-15 10:44:50 -0300

    Otimizacao de codigo

--- buscafilmes-app/src/app/components/navbar.jsx
+++ buscafilmes-app/src/app/components/navbar.jsx
@@ -1,52 +1,20 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
 
 import styles from "../css/navbar.css";
 import logo from "../images/logoIcon.png";
 
 export default function Navbar() {
-  const home = useRef(),
-    nowplaying = useRef(),
-    toprated = useRef(),
-    about = useRef();
-
-  const router = useRouter;
-
-  useEffect(() => {
-    switch (router.pathname) {
-      case "/":
-        home.class.toggle("active");
-        home.aria - current.toggle("page");
-        break;
-      case "/nowplaying":
-        about.class.toggle("active");
-        about.aria - current.toggle("page");
-        break;
-      case "/toprated":
-        about.class.toggle("active");
-        about.aria - current.toggle("page");
-        break;
-      case "/about":
-        about.class.toggle("active");
-        about.aria - current.toggle("page");
-        break;
-    }
-  }, []);
-
   return (
     <header>
-      <nav class="navbar navbar-expand-md fixed-top">
+      <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
           <Link class="navbar-brand" href="/">
             <Image
               src={logo}
               width="30"
               height="30"
-              class="d-inline-block align-top"
+              class="navbar-brand-img d-inline-block align-top"
               alt=""
             />
             BuscaFilmes
@@ -63,31 +31,26 @@ export default function Navbar() {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link ref={home} class="nav-link" href="/">
-                  Home
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link ref={nowplaying} class="nav-link" href="/nowplaying">
-                  Em alta
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link ref={toprated} class="nav-link" href="/toprated">
-                  Melhores avaliados
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link ref={about} class="nav-link" href="/about">
-                  Sobre
-                </Link>
-              </li>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-underline">
+              <NavLink href="/" name="Home" />
+              <NavLink href="/nowplaying" name="Em Alta" />
+              <NavLink href="/toprated" name="Melhores avaliados" />
+              <NavLink href="/about" name="Sobre" />
             </ul>
           </div>
         </div>
       </nav>
     </header>
+  );
+}
+
+function NavLink({ href, name, activePage }) {
+  const isActive = activePage === href;
+  return (
+    <li class={`nav-item ${isActive ? "active" : ""}`}>
+      <Link href={href} class="nav-link">
+        {name}
+      </Link>
+    </li>
   );
 }
--- buscafilmes-app/src/app/layout.jsx
+++ buscafilmes-app/src/app/layout.jsx
@@ -1,5 +1,4 @@
 import "./css/globals.css";
-import logo from "./images/logoIcon.png";
 import Navbar from "./components/navbar";
 
 export const metadata = {
@@ -28,7 +27,6 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         {children}
-
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
